7e71a6b7811c03e2f7200234e51ffc91bbd7af8b
huhao666 <782044678@qq.com>
2019-11-07 10:25:31 +0800

    修改文件结构，压缩js忽略min文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-const gulp = require('gulp'); 
-const uglify = require('gulp-uglify');
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const gulpif = require("gulp-if"); 
+
+
+var condition = function(f) {
+  if (f.path.indexOf(".min.js", f.path.length - ".min.js".length) !== -1) {
+    return false;
+  } 
+  return true;
+};
 
 //压缩js文件
-gulp.task('script', done => {
+gulp.task("jsmin", function() {
   //找到文件
-  gulp.src('js/*.js')
+  return gulp.src("src/js/*.js") 
     //压缩文件
-    .pipe(uglify())
-    //保存压缩后的文件
-    .pipe(gulp.dest('dist/js'));
-    done();
-})
+    .pipe(gulpif(condition, uglify()))
+    //保存压缩后的文件 
+    .pipe(gulp.dest("dist/js"));
+});
